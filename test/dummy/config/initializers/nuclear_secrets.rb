c07eeea40d01a846f11d36f869d5de3b2b10f39f
Nick Benoit <nick.benoit14@gmail.com>
2017-10-03 07:20:28 -0600

    extract errors

--- test/dummy/config/initializers/nuclear_secrets.rb
+++ test/dummy/config/initializers/nuclear_secrets.rb
@@ -1,7 +1,6 @@
 NuclearSecrets.configure do |config|
   config.required_secrets = {
-    #secret_key_base: String,
+    secret_key_base: String,
     secret_token: NilClass,
-    secret_key_base: Proc.new { false } 
   }
 end
